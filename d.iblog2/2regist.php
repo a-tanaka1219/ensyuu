cedde9fc446c21b24e61a1a0be0128296316fe04
a-tanaka1219 <amisa.jump.love.1130@gmail.com>
2023-06-26 21:59:06 +0900

    6.26

---
+++ d.iblog2/2regist.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>        
+        <meta charset="UTF-8">
+        <title>登録フォーム</title>
+        <link rel="stylesheet" type="text/css" href="regist1.css">
+    </head>
+    
+    <body>
+        <h1>登録フォーム</h1>
+    </body>
+</html>
