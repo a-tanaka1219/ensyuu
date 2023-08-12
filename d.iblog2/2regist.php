7d8fb7b42433a758ccf5feb318c8441540c23ac7
a-tanaka1219 <amisa.jump.love.1130@gmail.com>
2023-08-12 22:37:01 +0900

    8.12

--- d.iblog2/2regist.php
+++ d.iblog2/2regist.php
@@ -1,20 +1,87 @@
 <?php
-var_dump($_POST);
+//var_dump($_POST);
 //$errorの文字列がから（初期化）
-$error="";
+$error1="";
 //$_POST['family_name']がセットされていて、値がからの時
 if(isset($_POST['family_name']) && $_POST['family_name']==""){
     //$errorに文字列を代入する
-    $error = "名前（姓）を入力してください";
+    $error1 = "名前（姓）を入力してください";
     }
 else if(isset($_POST['family_name']) && mb_strlen($_POST['family_name']) > 10){
-    $error = "１０文字以内で入力してください";
+    $error1 = "１０文字以内で入力してください";
+//}else if(isset($_POST['family_name']) && !preg_match('/[ぁ-ん亜-熙]/',$_POST['family_name'])){
+    //$error = "ひらがな、漢字のみで入力してください";
 }
-else if(isset($_POST['family_name']) && !preg_match('/[ぁ-ん亜-熙]/',$_POST['family_name'])){
-    $error = "ひらがな、漢字のみで入力してください";
-}
-var_dump($error);
 
+$error2="";
+if(isset($_POST['last_name']) && $_POST['last_name']==""){
+    $error2 = "名前（名）を入力してください";
+}else if(isset($_POST['last_name']) && mb_strlen($_POST['last_name']) > 10){
+    $error2 = "１０文字以内で入力してください";
+}
+
+$error3="";
+if(isset($_POST['family_name_kana']) && $_POST['family_name_kana']==""){
+    $error3 = "カナ（姓）を入力してください";
+}else if(isset($_POST['family_name_kana']) && mb_strlen($_POST['family_name_kana']) > 10){
+    $error3 = "１０文字以内で入力してください";
+}else if(isset($_POST['family_name_kana']) && preg_match('/[ァ-ヴ]/',$_POST['family_name_kana'])){
+    $error3 = "カタカナのみで入力してください";
+}
+
+$error4="";
+if(isset($_POST['last_name_kana']) && $_POST['last_name_kana']==""){
+    $error4 = "カナ（名）を入力してください";
+}else if(isset($_POST['last_name_kana']) && mb_strlen($_POST['last_name_kana']) > 10){
+    $error4 = "１０文字以内で入力してください";
+}
+
+$error5="";
+if(isset($_POST['mail']) && $_POST['mail']==""){
+    $error5 = "メールアドレスを入力してください";
+}else if(isset($_POST['mail']) && strlen($_POST['mail']) > 100){
+    $error5 = "１００文字以内で入力してください";
+}
+
+$error6="";
+if(isset($_POST['password']) && $_POST['password']==""){
+    $error6 = "パスワードを入力してください";
+}else if(isset($_POST['password']) && strlen($_POST['password']) > 10){
+    $error6 = "１０文字以内で入力してください";
+}
+
+$error7="";
+if(isset($_POST['post_code']) && $_POST['post_code']==""){
+    $error7 = "郵便番号を入力してください";
+}else if(isset($_POST['post_code']) && strlen($_POST['post_code']) > 7){
+    $error7 = "７文字以内で入力してください";
+}
+
+//var_dump($_POST['prefecture']);
+$error8="";
+//都道府県が選択されていない（初期値の状態）時エラーを表示
+if(isset($_POST['prefecture']) && $_POST['prefecture']==""){
+    $error8 = "都道府県を選択してください";
+}
+var_dump($error8);
+    
+$error9 ="";
+if(isset($_POST['address_1']) && $_POST['address_1']==""){
+    $error9 = "住所（市町村）を入力してください";
+}else if(isset($_POST['address_1']) && mb_strlen($_POST['address_1']) > 10){
+    $error9 = "１０文字以内で入力してください";
+}
+
+$error10 = "";
+if(isset($_POST['address_2']) && $_POST['address_2']==""){
+    $error10 = "住所（番地）を入力してください";
+}else if(isset($_POST['address_2']) && mb_strlen($_POST['address_2']) > 100){
+    $error = "１００文字以内で入力してください";
+}
+
+
+//var_dump($error1);
+//var_dump($error3);
 
 //$_POSTが空ではなく、$errorが空の時
 if(!empty($_POST) && empty($error)){
@@ -42,12 +109,14 @@ if(!empty($_POST) && empty($error)){
                                   echo $_POST['family_name'];
                                   }
                               ?>">
+                
                 <?php
                 //エラーの表示はここ
-                if(!empty($error)){
-                    echo $error;
+                if(!empty($error1)){
+                    echo "<p style = 'color:red;'>".$error1;
                 }
                 ?>
+                
             </div>
             <div>
                 <label>名前（名）</label>
@@ -57,6 +126,13 @@ if(!empty($_POST) && empty($error)){
                                   echo $_POST['last_name'];
                                   }
                                 ?>">
+                
+                <?php
+                if(!empty($error2)){
+                    echo "<p style = 'color:red;'>".$error2;
+                }
+                ?>
+                
             </div>
             <div>
                 <label>カナ（姓）</label>
@@ -66,6 +142,13 @@ if(!empty($_POST) && empty($error)){
                                   echo $_POST['family_name_kana'];
                               }
                               ?>">
+                
+                <?php
+                if(!empty($error3)){
+                    echo "<p style = 'color:red;'>".$error3;
+                }
+                ?>
+                
             </div>
             <div>
                 <label>カナ（名）</label>
@@ -75,6 +158,13 @@ if(!empty($_POST) && empty($error)){
                                   echo $_POST['last_name_kana'];
                               }
                               ?>">
+                
+                <?php
+                if(!empty($error4)){
+                    echo "<p style = 'color:red;'>".$error4;
+                }
+                ?>
+                
             </div>
             <div>
                 <label>メールアドレス</label>
@@ -84,10 +174,22 @@ if(!empty($_POST) && empty($error)){
                                   echo $_POST['mail'];
                               }
                               ?>">
+                
+                <?php
+                if(!empty($error5)){
+                    echo "<p style = 'color:red;'>".$error5;
+                }
+                ?>
+                
             </div>
             <div>
                 <label>パスワード</label>
                 <input type="password" size="35" name="password">
+                <?php
+                if(!empty($error6)){
+                    echo "<p style = 'color:red;'>".$error6;
+                }
+                ?>
             </div>
             <div>
                 <label>性別</label>
@@ -111,12 +213,17 @@ if(!empty($_POST) && empty($error)){
                               if(!empty($_POST['post_code'])){
                                   echo $_POST['post_code'];
                               }
-                              ?>"> 
+                              ?>">
+                <?php
+                if(!empty($error7)){
+                    echo "<p style = 'color:red;'>".$error7;
+                }
+                ?>
             </div>
             <div>
                 <label>住所（都道府県）</label>
                 <select name="prefecture">
-                    <option value="都道府県">都道府県</option>
+                    <option value="">都道府県</option>
                     
                     <?php
                     $todohuken=array("北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県","茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県","新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県","静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県","徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県","沖縄県");
@@ -131,7 +238,14 @@ if(!empty($_POST) && empty($error)){
                     }
                     ?>
                     
+                    
                 </select>
+                    <?php
+                    if(!empty($error8)){
+                        echo "<p style = 'color:red;'>".$error8."</p>";
+                    }
+                    
+                    ?>
             </div>
             <div>
                 <label>住所（市町村）</label>
@@ -141,6 +255,11 @@ if(!empty($_POST) && empty($error)){
                                   echo $_POST['address_1'];
                               }
                               ?>">
+                <?php
+                if(!empty($error9)){
+                    echo "<p style = 'color:red;'>".$error9;
+                }
+                ?>
             </div>
             <div>
                 <label>住所（番地）</label>
@@ -150,6 +269,12 @@ if(!empty($_POST) && empty($error)){
                                   echo $_POST['address_2'];
                               }
                               ?>">
+                
+                <?php
+                if(!empty($error10)){
+                    echo "<p style = 'color:red;'>".$error10;
+                }
+                ?>
             </div>
             <div>
                 <label>アカウント権限</label>
